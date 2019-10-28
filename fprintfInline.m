76dbdce085d110f5de04d5e2d7692668af873bbb
acaccavano <52933495+acaccavano@users.noreply.github.com>
2019-10-28 15:47:30 -0400

    no changes

--- fprintfInline.m
+++ fprintfInline.m
@@ -1,14 +1,14 @@
-
-
 function formatSpec = fprintfInline(txtLabel,nIterations)
-  % build formatSpec: put necessary backspaces in, define format specifier (leftalign)
+  
+% build formatSpec: put necessary backspaces in, define format specifier (leftalign)
 
-  nBlanks = floor(log10(nIterations))+1;
-  fprintf([txtLabel blanks(nBlanks)]);
-  backString = '';
+nBlanks = floor(log10(nIterations))+1;
+fprintf([txtLabel blanks(nBlanks)]);
+backString = '';
 
-  for n = 1:nBlanks
-      backString = strcat(backString,'\b');
-  end
-  formatSpec = [backString '%-' num2str(nBlanks) 'd'];
+for n = 1:nBlanks
+  backString = strcat(backString,'\b');
+end
+formatSpec = [backString '%-' num2str(nBlanks) 'd'];
+
 end
