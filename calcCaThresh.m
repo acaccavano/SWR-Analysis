5956a6b69a32d258cf4f0f98bef9cd5a55c79c2f
acaccavano <52933495+acaccavano@users.noreply.github.com>
2019-11-19 11:24:21 -0500

    removed trimming of tSeries to new function trimCaFile, as logic was getting complicated for stimulation windows

--- calcCaThresh.m
+++ calcCaThresh.m
@@ -1,29 +1,20 @@
-function [baseMean, baseSD, baseThresh, peakThresh] = calcCaThresh(tSeries, timing, param)
+function [baseMean, baseSD, baseThresh, peakThresh] = calcCaThresh(tSeries, param)
 
-if (nargin < 3) param = struct; end
+if (nargin < 2) param = struct; end
 
-if ~isfield(param,'sdMult')        param.sdMult        = 5;    end
-if ~isfield(param,'sdBaseFactor')  param.sdBaseFactor  = 0.6; end
-if ~isfield(param,'skipDetectLim') param.skipDetectLim = 2;    end
+if ~isfield(param,'sdMult')        param.sdMult        = 4;    end
+if ~isfield(param,'sdBaseFactor')  param.sdBaseFactor  = 0.8;  end
 
 % Test if tSeries is cell array, if so it is array of files with consistent baselines
 if iscell(tSeries)
   [~, nCols] = cellfun(@size,tSeries);
   
+  % Concatenate into one continuous file if same number of cells
   if (all(nCols == nCols(1)))
-    % Remove from threshold detection periods before param.skipDetectLim
-    for i = 1:length(tSeries)
-      tSeries{i} = tSeries{i}(timing{i} >= 1000 * param.skipDetectLim, :);
-    end
-    
-    % concatenate into one continuous file:
     tSeries = vertcat(tSeries{:});
   else
     error('Array mismatch. Ensure all files have same number of cells to use consistent baseline');
   end
-else
-  % Remove from threshold detection periods before param.skipDetectLim
-  tSeries = tSeries(timing >= 1000 * param.skipDetectLim, :);
 end
 
 % tSeries diminsion > 1 signifies multiple cell recording (Ca imaging)
