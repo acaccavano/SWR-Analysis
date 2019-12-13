774da1bffe4a57cf3a893f4352aa8cb18b8d1c49
acaccavano <52933495+acaccavano@users.noreply.github.com>
2019-12-13 16:28:06 -0500

    added inaccessible gaussian fitting params to UI and param structure. they were optimized for 40x spontaneous recordings, but they need to be tweaked for different objectives and stim files where dF/F has greater magnitude

--- calcCaBaseline.m
+++ calcCaBaseline.m
@@ -1,10 +1,15 @@
 function [baseMean, baseSD] = calcCaBaseline(tSeries, param) % [baseMean, baseSD]
 
 if (nargin < 2) param = struct; end
-if ~isfield(param,'baseDetectMethod') param.baseDetectMethod  = 2; end
-if ~isfield(param,'baseQuant')   param.baseQuant   = 0.8; end
-if ~isfield(param,'minBaseline') param.minBaseline = 0.1; end
-if ~isfield(param,'plotHisto')   param.plotHisto   = 0;   end
+if ~isfield(param,'baseDetectMethod') param.baseDetectMethod = 2;    end
+if ~isfield(param,'baseQuant')        param.baseQuant        = 0.8;  end
+if ~isfield(param,'minBaseline')      param.minBaseline      = 0.1;  end
+if ~isfield(param,'pkDiffMin')        param.pkDiffMin        = 0.1;  end % How close peaks can be before considered eqivalent = abs(B1-B2)
+if ~isfield(param,'pkSimLim')         param.pkSimLim         = 2;    end % Peak similarity metric = (A1^2 + A2^2)/(A1*A2)
+if ~isfield(param,'kurtosisMin')      param.kurtosisMin      = 0;    end % Min kurtosis limit to fit with 2 gaussians (otherwise skip 1st fit)
+if ~isfield(param,'kurtosisMax')      param.kurtosisMax      = 5;    end % Max kurtosis limit until exclude high points (otherwise fit can fail)
+if ~isfield(param,'excludeQuant')     param.excludeQuant     = 0.98; end % quantile to limit 1st fit to if kurtosis limit exceeded
+if ~isfield(param,'plotFitHisto')     param.plotFitHisto     = 1;    end
 
 if (param.baseDetectMethod == 1)
   tSeries(tSeries > quantile(tSeries, param.baseQuant)) = [];
@@ -12,16 +17,8 @@ if (param.baseDetectMethod == 1)
   baseSD   = std(tSeries);
   
 elseif (param.baseDetectMethod == 2)
-  
-  % Fitting parameter metrics
-  pkDiffMin    = 0.1; % How close peaks can be before considered eqivalent = abs(B1-B2)
-  pkSimLim     = 2; % Peak similarity metric = (A1^2 + A2^2)/(A1*A2)
-  kurtosisMax  = 5; % Max kurtosis limit until exclude high points (otherwise fit can fail)
-  kurtosisMin  = 0; % Min kurtosis limit to fit with 2 gaussians (otherwise skip 1st fit)
-  excludeQuant = 0.98; % quantile to limit 1st fit to if kurtosis limit exceeded
-  
+
   % Bin all data across tSeries:
-  
   % Ensure  there are at least 200 bins per 1 dF/F:
   nBins = max(floor(200*range(tSeries)), 200);
   [binValues, binEdges] = histcounts(tSeries, nBins);
@@ -32,38 +29,38 @@ elseif (param.baseDetectMethod == 2)
   yVal = binValues';
   exRange = zeros(length(xVal), 1);
   
-  if param.plotHisto
+  if param.plotFitHisto
     figure
     hold on
   end
 
-  if kurtosis(tSeries) > kurtosisMin
+  if kurtosis(tSeries) > param.kurtosisMin
       
     ft = fittype('gauss2');
     loParams = [0 min(tSeries) 0 0 min(tSeries) 0];
     hiParams = [Inf max(tSeries) range(tSeries) Inf max(tSeries) range(tSeries)];
     weightEq = exp(-10*linspace(1,nBins,nBins)'/nBins);
     
-    if kurtosis(tSeries) > kurtosisMax
-      hiLimX = quantile(tSeries, excludeQuant);
+    if kurtosis(tSeries) > param.kurtosisMax
+      hiLimX = quantile(tSeries, param.excludeQuant);
       exRange = xVal > hiLimX;
     end
     
     f = fit(xVal, yVal, ft, 'Lower', loParams, 'Upper', hiParams, 'Robust', 'Bisquare', 'Weights', weightEq, 'Exclude', exRange);
     
-    if param.plotHisto 
+    if param.plotFitHisto 
       plot(f, xVal, yVal, exRange) 
     end
 
     pkSimMetric   = sqrt((f.a1^2 + f.a2^2)/(f.a1*f.a2));
     
     % Special case where peaks are on top of eachother (often when noise >> signal):
-    if abs(f.b1 - f.b2) < pkDiffMin
+    if abs(f.b1 - f.b2) < param.pkDiffMin
       % Use average of fits to define BL range:
       hiLimX = mean([f.b1 f.b2]) + mean([f.c1 f.c2])/sqrt(2);
       
       % Special case when peaks are similar amplitude (often when noise similar of < signal):
-    elseif pkSimMetric < pkSimLim
+    elseif pkSimMetric < param.pkSimLim
       % Use more negative peak to define BL range:
       if (f.b1 < f.b2)
         hiLimX = f.b1 + f.c1/sqrt(2);
@@ -91,7 +88,7 @@ elseif (param.baseDetectMethod == 2)
   baseMean = f.b1;
   baseSD   = f.c1/sqrt(2);
   
-  if param.plotHisto 
+  if param.plotFitHisto 
     plot(f, xVal, yVal, exRange) 
   end 
   
