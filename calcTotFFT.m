08df272e478ea564be0b9d38b71851d001519417
acacc <acacc@10.177.37.42>
2021-03-09 16:26:51 -0500

    add gaussian fit (requires Curve Fitting Toolbox) to calculate mean (ie new pkFreq measure), SD, and FWHM of frequency peak. FWHM calculated from fit sigma, not from data - which for flat responses would fail for many recordings.

--- calcTotFFT.m
+++ calcTotFFT.m
@@ -4,8 +4,9 @@ function S = calcTotFFT(S, param)
 %  Function to create new structure and calculate fast-fourier transform
 %  (FFT) over the total duration of a signal.
 
-if ~isfield(S,'FFT')        S.FFT = struct;      end
-if ~isfield(param,'fftRes') param.fftRes = 0.1;  end % desired frequency resolution
+if ~isfield(S,'FFT')            S.FFT = struct;           end
+if ~isfield(param,'fftRes')     param.fftRes = 0.1;       end % desired frequency resolution
+if ~isfield(param,'plotFitFFT') param.plotFitFFT = false; end % plot gaussian fit
 
 % (Re)Initialize data arrays:
 S.FFT.F   = [];
@@ -39,8 +40,25 @@ S.FFT.F1 = 2*S.FFT.F1(1:nfft/2+1);     % Multiply the values by 2 --> two spectr
 S.FFT.F1(1) = S.FFT.F1(1)/2;             % Only the first nfft/2+1 values needed because of multiplication by 2
 
 S.FFT.subRange = find(S.FFT.fftRange>S.lim1 & S.FFT.fftRange<S.lim2);
+
+% Simple peak determination - unreliable, very sensitive to noise
 [~, pkFreqInd] = max(S.FFT.F1(S.FFT.subRange));
 S.FFT.pkFreq = S.FFT.fftRange(S.FFT.subRange(pkFreqInd));
+
+% Gaussian fit peak determination
+ft = fittype('gauss1');
+loParams = [0 min(S.FFT.fftRange(S.FFT.subRange)) 0];
+hiParams = [Inf max(S.FFT.fftRange(S.FFT.subRange)) range(S.FFT.fftRange(S.FFT.subRange))];
+
+S.FFT.fitGauss = fit(S.FFT.fftRange(S.FFT.subRange)', S.FFT.F1(S.FFT.subRange), ft, 'Lower', loParams, 'Upper', hiParams, 'Robust', 'Bisquare');
+
+S.FFT.fitMean = S.FFT.fitGauss.b1;
+S.FFT.fitSD   = S.FFT.fitGauss.c1/sqrt(2);
+S.FFT.fitFWHM = 2 * sqrt(2 * log(2)) * S.FFT.fitSD;
+
+if param.plotFitFFT
+  plot(S.FFT.fitGauss, S.FFT.fftRange(S.FFT.subRange)', S.FFT.F1(S.FFT.subRange))
+end
 
 S.FFT = orderStruct(S.FFT);
 
