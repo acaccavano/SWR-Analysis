524bd201b22f04cbb18cb716459e4e176389ccfc
acacc <acacc@10.177.37.42>
2021-03-09 14:13:59 -0500

    working now, fixed bug with frequency conversion. huge increase in performance - shifted to sample Loc values instead of timing values, no longer necessary to use find functions to create piecewise linear function. only detects max values based on parameters, and finds global min between two detected maxes.

--- calcTotPhase.m
+++ calcTotPhase.m
@@ -1,4 +1,4 @@
-function S = calcTotPhase(S, LFP)
+function S = calcTotPhase(S, LFP, param)
 %% S = calcTotPhase(S, LFP)
 %
 %  Function to calculate the phase for specific frequency range of signal S using a
@@ -7,8 +7,9 @@ function S = calcTotPhase(S, LFP)
 %  Requires LFP structure for timing
 
 if ~isfield(S,'phase') S.phase = struct; end
-
-splineOption = false;
+if ~isfield(param,'minPkDiffFrac') param.minPkDiffFrac = 1.5;  end % Fraction of max frequency (lim2) to set min peak difference for exclusion of events. 1 = max frequency of band, is pretty conservative, default = 1.5, so two successive oscillations can be slightly faster than allowed
+if ~isfield(param,'minPkPromFrac') param.minPkPromFrac = 0.0;  end % Fraction of SD of signal to set Min peak prominence (zero to ignore as criteria)
+if ~isfield(param,'samplingInt')   param.samplingInt = LFP.timing(2) - LFP.timing(1); end
 
 % (Re)Initialize data arrays:
 S.phase.maxVal  = [];
@@ -20,63 +21,42 @@ S.phase.nCycle  = [];
 S.phase.phFreq  = [];
 
 %% Compute oscillation phase:
-[S.phase.maxVal, S.phase.maxLoc] = findpeaks(S.tSeries, LFP.timing, 'MinPeakDistance', 1/S.lim2);
-[S.phase.minVal, S.phase.minLoc] = findpeaks(-S.tSeries, LFP.timing, 'MinPeakDistance', 1/S.lim2);
-S.phase.minVal = -S.phase.minVal;
+minPkDiff = 1000/(param.minPkDiffFrac * S.lim2 * param.samplingInt);
+minPkProm = param.minPkPromFrac * std(S.tSeries);
 
-if ~splineOption % Use linear interpolation
-  S.phase.tPhase = zeros(length(S.tSeries), 1);
+[S.phase.maxVal, S.phase.maxLoc] = findpeaks(S.tSeries, 'MinPeakDistance', minPkDiff, 'MinPeakProminence', minPkProm);
+S.phase.minVal = NaN * ones(length(S.phase.maxVal) - 1, 1);
+S.phase.minLoc = NaN * ones(length(S.phase.maxVal) - 1, 1);
+
+% Piecewise Linear Interpolation
+S.phase.tPhase = zeros(length(S.tSeries), 1);
+
+formatSpec = fprintfInline('interpolating piecewise linear phase function for peak =  ', length(S.phase.maxLoc)-1);
+for i = 1 : length(S.phase.maxLoc) - 1
   
-  if S.phase.minLoc(1) < S.phase.maxLoc(1)
-    S.phase.minLoc(1) = [];
-    S.phase.minVal(1) = [];
-  end
+  [S.phase.minVal(i), S.phase.minLoc(i)] = min(S.tSeries(S.phase.maxLoc(i) : S.phase.maxLoc(i+1)));
+  S.phase.minLoc(i) = S.phase.minLoc(i) + S.phase.maxLoc(i);
   
-  for i = 1 : length(S.phase.maxLoc)-1
-    tSubArray1 = find(LFP.timing>=S.phase.maxLoc(i) & LFP.timing<=S.phase.minLoc(i));
-    tSubArray2 = find(LFP.timing>=S.phase.minLoc(i) & LFP.timing<=S.phase.maxLoc(i+1));
-    S.phase.tPhase(tSubArray1) = linspace(0, pi, length(tSubArray1));
-    S.phase.tPhase(tSubArray2) = linspace(pi, 2*pi, length(tSubArray2));
-  end
-  
-else % Use cubic spline interpolation - CAUTION: only works for very well-defined oscillations
-  if S.phase.maxLoc(1) < S.phase.minLoc(1)
-    phaseMax = pi*2*(0:length(S.phase.maxLoc)-1);
-  else
-    phaseMax = pi*2*(1:length(S.phase.maxLoc));
-  end
-  phaseMin = pi*(2*(1:length(S.phase.minLoc))-1);
-  phase1 = sort([phaseMax, phaseMin])';
-  
-  S.phase.tPhase = mod(spline(sort([S.phase.maxLoc;S.phase.minLoc]), phase1, LFP.timing),2*pi);
+  tSubArray1 = S.phase.maxLoc(i) : S.phase.minLoc(i);
+  tSubArray2 = S.phase.minLoc(i) : S.phase.maxLoc(i+1);
+  S.phase.tPhase(tSubArray1) = linspace(0, pi, length(tSubArray1));
+  S.phase.tPhase(tSubArray2) = linspace(pi, 2*pi, length(tSubArray2));
+  fprintf(formatSpec,i);
 end
+fprintf('...done\n');
 
-%% Calculate number of cycles and frequency
-firstPeak = find(S.phase.maxLoc(1) == LFP.timing);
-lastPeak = find(S.phase.maxLoc(end) == LFP.timing);
-
-if isempty(firstPeak) firstPeak = 1; end
-if isempty(lastPeak) lastPeak = length(LFP.timing); end
-
-contPhase = S.phase.tPhase(firstPeak:lastPeak);
-nCycle = 0;
-
-for i = 1:length(contPhase)
-  if (contPhase(i) == 0) nCycle = nCycle + 1; end
-  contPhase(i) = contPhase(i) + pi*2*nCycle;
-end
-
-S.phase.nCycle = (contPhase(end) - contPhase(1)) / (2*pi);
-S.phase.phFreq = S.phase.nCycle / ((LFP.timing(end) - LFP.timing(1))/1000);
+% Calculate number of cycles and frequency
+S.phase.nCycle = length(S.phase.maxLoc) - 1;
+S.phase.phFreq = S.phase.nCycle / ((LFP.timing(S.phase.maxLoc(end)) - LFP.timing(S.phase.maxLoc(1)))/1000);
 
 % Error handling: limit phase frequency in case it exceeds frequency bounds
-if S.phase.phFreq < S.lim1
-  S.phase.phFreq = S.lim1;
-  S.phase.nCycle = S.phase.phFreq * ((LFP.timing(end) - LFP.timing(1))/1000);
-elseif S.phase.phFreq > S.lim2
-  S.phase.phFreq = S.lim2;
-  S.phase.nCycle = S.phase.phFreq * ((LFP.timing(end) - LFP.timing(1))/1000);
-end
+% if S.phase.phFreq < S.lim1
+%   S.phase.phFreq = S.lim1;
+%   S.phase.nCycle = S.phase.phFreq * ((LFP.timing(end) - LFP.timing(1))/1000);
+% elseif S.phase.phFreq > S.lim2
+%   S.phase.phFreq = S.lim2;
+%   S.phase.nCycle = S.phase.phFreq * ((LFP.timing(end) - LFP.timing(1))/1000);
+% end
 
 S.phase = orderStruct(S.phase);
 
